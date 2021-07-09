84834e14833042e3c6107f3f55973e94b5cdb129
David Bailey <davidbailey00@outlook.com>
2021-07-09 18:17:17 +0100

    Add placeholder pull-to-refresh on Journeys page

--- app/src/main/java/co/zimly/bytrain/screens/Journeys.kt
+++ app/src/main/java/co/zimly/bytrain/screens/Journeys.kt
@@ -10,7 +10,7 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Map
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -30,111 +30,135 @@ import co.zimly.bytrain.composables.Section
 import co.zimly.bytrain.composables.TitleText
 import co.zimly.bytrain.model.Screen
 import co.zimly.bytrain.sheets.ByTrainPro
+import com.google.accompanist.swiperefresh.SwipeRefresh
+import com.google.accompanist.swiperefresh.rememberSwipeRefreshState
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @ExperimentalMaterialApi
 @Composable
 fun Journeys(navController: NavController) {
+    val scope = rememberCoroutineScope()
+    var isRefreshing by remember { mutableStateOf(false) }
+
+    SwipeRefresh(
+        state = rememberSwipeRefreshState(isRefreshing),
+        onRefresh = {
+            isRefreshing = true
+            scope.launch {
+                delay(500)
+                isRefreshing = false
+            }
+        }
+    ) {
+        Column(
+            Modifier
+                .verticalScroll(rememberScrollState())
+                .padding(16.dp)
+        ) {
+            MainContent(navController)
+        }
+    }
+}
+
+@ExperimentalMaterialApi
+@Composable
+private fun MainContent(navController: NavController) {
     val sheetController = LocalSheetController.current
     sheetController.BackHandler()
 
-    Column(
-        Modifier
-            .verticalScroll(rememberScrollState())
-            .padding(16.dp)
-    ) {
-        TitleText(stringResource(R.string.journeys))
+    TitleText(stringResource(R.string.journeys))
 
-        Section(stringResource(R.string.upcoming)) {
-            Card(Modifier.fillMaxWidth(), contentColor = MaterialTheme.colors.onSurface) {
-                Column(
-                    Modifier
-                        .padding(16.dp)
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                ) {
-                    Icon(Icons.Filled.Map, contentDescription = null)
-                    Spacer(Modifier.height(4.dp))
-                    Text(
-                        stringResource(R.string.no_journeys),
-                        fontSize = 18.sp,
-                        fontWeight = FontWeight.Medium,
-                    )
-                    Text(
-                        stringResource(R.string.planner_cta),
-                        textAlign = TextAlign.Center,
-                    )
-                    Spacer(Modifier.height(12.dp))
-                    Button(
-                        onClick = {
-                            navController.navigate(Screen.Planner.route) {
-                                popUpTo(Screen.Journeys.route) {
-                                    saveState = true
-                                }
-                                launchSingleTop = true
-                                restoreState = true
+    Section(stringResource(R.string.upcoming)) {
+        Card(Modifier.fillMaxWidth(), contentColor = MaterialTheme.colors.onSurface) {
+            Column(
+                Modifier
+                    .padding(16.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Icon(Icons.Filled.Map, contentDescription = null)
+                Spacer(Modifier.height(4.dp))
+                Text(
+                    stringResource(R.string.no_journeys),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Medium,
+                )
+                Text(
+                    stringResource(R.string.planner_cta),
+                    textAlign = TextAlign.Center,
+                )
+                Spacer(Modifier.height(12.dp))
+                Button(
+                    onClick = {
+                        navController.navigate(Screen.Planner.route) {
+                            popUpTo(Screen.Journeys.route) {
+                                saveState = true
                             }
-                        },
-                        Modifier.fillMaxWidth()
-                    ) {
-                        Icon(Icons.Filled.DateRange, contentDescription = null)
-                        Spacer(Modifier.width(8.dp))
-                        Text(stringResource(R.string.start_planning))
-                    }
+                            launchSingleTop = true
+                            restoreState = true
+                        }
+                    },
+                    Modifier.fillMaxWidth()
+                ) {
+                    Icon(Icons.Filled.DateRange, contentDescription = null)
+                    Spacer(Modifier.width(8.dp))
+                    Text(stringResource(R.string.start_planning))
                 }
             }
         }
+    }
 
-        Spacer(Modifier.height(8.dp))
+    Spacer(Modifier.height(8.dp))
 
-        Section(title = stringResource(R.string.featured)) {
-            Card(
-                onClick = {
-                    sheetController.present { ByTrainPro() }
-                },
-                Modifier.fillMaxWidth(),
-                contentColor = MaterialTheme.colors.onSurface,
-                role = Role.Button
+    Section(title = stringResource(R.string.featured)) {
+        Card(
+            onClick = {
+                sheetController.present { ByTrainPro() }
+            },
+            Modifier.fillMaxWidth(),
+            contentColor = MaterialTheme.colors.onSurface,
+            role = Role.Button
+        ) {
+            Column(
+                Modifier
+                    .padding(24.dp)
+                    .fillMaxWidth(),
+                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
-                Column(
-                    Modifier
-                        .padding(24.dp)
-                        .fillMaxWidth(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                val byTrainPro = stringResource(R.string.bytrain_pro)
+                Row(
+                    Modifier.semantics { contentDescription = byTrainPro },
+                    verticalAlignment = Alignment.CenterVertically
                 ) {
-                    val byTrainPro = stringResource(R.string.bytrain_pro)
-                    Row(
-                        Modifier.semantics { contentDescription = byTrainPro },
-                        verticalAlignment = Alignment.CenterVertically
-                    ) {
-                        Image(
-                            painterResource(R.drawable.icon),
-                            contentDescription = null,
-                            Modifier.size(28.dp),
-                        )
-                        Spacer(Modifier.width(8.dp))
-                        Text(
-                            stringResource(R.string.app_name),
-                            color = MaterialTheme.colors.onBackground,
-                            fontSize = 24.sp,
-                            fontWeight = FontWeight.ExtraBold,
-                        )
-                        Spacer(Modifier.width(4.dp))
-                        Text(
-                            stringResource(R.string.pro_caps),
-                            Modifier
-                                .clip(RoundedCornerShape(8.dp))
-                                .background(MaterialTheme.colors.primary)
-                                .padding(horizontal = 6.dp, vertical = 1.dp),
-                            color = MaterialTheme.colors.onPrimary,
-                            fontWeight = FontWeight.Bold,
-                        )
-                    }
-                    Spacer(Modifier.height(8.dp))
+                    Image(
+                        painterResource(R.drawable.icon),
+                        contentDescription = null,
+                        Modifier.size(28.dp),
+                    )
+                    Spacer(Modifier.width(8.dp))
                     Text(
-                        stringResource(R.string.pro_cta),
-                        textAlign = TextAlign.Center,
+                        stringResource(R.string.app_name),
+                        color = MaterialTheme.colors.onBackground,
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.ExtraBold,
+                    )
+                    Spacer(Modifier.width(4.dp))
+                    Text(
+                        stringResource(R.string.pro_caps),
+                        Modifier
+                            .clip(RoundedCornerShape(8.dp))
+                            .background(MaterialTheme.colors.primary)
+                            .padding(horizontal = 6.dp, vertical = 1.dp),
+                        color = MaterialTheme.colors.onPrimary,
+                        fontWeight = FontWeight.Bold,
                     )
                 }
+                Spacer(Modifier.height(8.dp))
+                Text(
+                    stringResource(R.string.pro_cta),
+                    textAlign = TextAlign.Center,
+                )
             }
         }
     }
